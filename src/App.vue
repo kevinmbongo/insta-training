6c408ec37aa1c3486861e005eab57488e1236296
kevinmbongo <mbongo.kevin.km@gmail.com>
2021-04-17 00:13:51 +0200

    feat(project): creat header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,115 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <!-- ***top-nav Oney *** -->
+    <nav id="oney-nav">
+      <div id="logo-oney">
+        <img
+          id="logo-oney-img"
+          src="https://upload.wikimedia.org/wikipedia/fr/thumb/3/38/Logo_Oney.svg/1280px-Logo_Oney.svg.png"
+          alt="logo oney"
+        />
+      </div>
+
+      <div id="top-nav-menu">
+        <div>
+          <a href="">Paiments </a>
+          <img class="icon" src="https://www.fine-s.fr/9959/test.jpg" alt="" />
+        </div>
+        <div>
+          <a href="">Crédits </a>
+          <img class="icon" src="https://www.fine-s.fr/9959/test.jpg" alt="" />
+        </div>
+        <div>
+          <a href=""> Assurances </a>
+          <img class="icon" src="https://www.fine-s.fr/9959/test.jpg" alt="" />
+        </div>
+        <div>
+          <a href=""> Nous découvrir </a>
+          <img class="icon" src="https://www.fine-s.fr/9959/test.jpg" alt="" />
+        </div>
+        <div>|</div>
+        <div style="font-family: Arial-Bold">
+          <a href=""> Créer un compte </a>
+          <img class="icon" src="https://www.fine-s.fr/9959/test.jpg" alt="" />
+        </div>
+        <a class="login-button" href="">
+          <img class="icon" src="https://www.fine-s.fr/9959/test.jpg" alt="" />
+          Connexion
+        </a>
+      </div>
+    </nav>
+
+    <!-- ***top green nav *** -->
+
+    <nav id="green-nav">
+      <div>Informations légales</div>
+    </nav>
+
+    <!-- *** menu choice *** -->
+
+    <nav id="choice-nav">
+      <div id="div-envelopante">
+        <div class="pop">
+          <div class="flex-wrap">
+            <img
+              class="icon"
+              src="https://www.fine-s.fr/9959/test.jpg"
+              alt=""
+            />
+            <p>Mention légal</p>
+          </div>
+          <div class="flex-wrap">
+            <img
+              class="icon"
+              src="https://www.fine-s.fr/9959/test.jpg"
+              alt=""
+            />
+            <p>Mention légal</p>
+          </div>
+        </div>
+
+        <div>
+          <div class="flex-wrap">
+            <img
+              class="icon"
+              src="https://www.fine-s.fr/9959/test.jpg"
+              alt=""
+            />
+            <p>Mention légal</p>
+          </div>
+          <div class="flex-wrap">
+            <img
+              class="icon"
+              src="https://www.fine-s.fr/9959/test.jpg"
+              alt=""
+            />
+            <p>Mention légal</p>
+          </div>
+        </div>
+
+        <div>
+          <div class="flex-wrap">
+            <img
+              class="icon"
+              src="https://www.fine-s.fr/9959/test.jpg"
+              alt=""
+            />
+            <p>Mention légal</p>
+          </div>
+          <div class="flex-wrap">
+            <img
+              class="icon"
+              src="https://www.fine-s.fr/9959/test.jpg"
+              alt=""
+            />
+            <p>Mention légal</p>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </nav>
+  </header>
+
+  <main></main>
+  <footer></footer>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
