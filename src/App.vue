a41ba684a8acddb3831e998c1fe6c47e76a7d575
kevinmbongo <mbongo.kevin.km@gmail.com>
2021-04-17 04:04:44 +0200

    feature(project): add main

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@
         </div>
         <a class="login-button" href="">
           <img class="icon" src="https://www.fine-s.fr/9959/test.jpg" alt="" />
-          Connexion
+          <p class="space-start">Connexion</p>
         </a>
       </div>
     </nav>
@@ -56,7 +56,7 @@
               src="https://www.fine-s.fr/9959/test.jpg"
               alt=""
             />
-            <p>Mention légal</p>
+            <p class="space-start">Mention légal</p>
           </div>
           <div class="flex-wrap">
             <img
@@ -64,26 +64,7 @@
               src="https://www.fine-s.fr/9959/test.jpg"
               alt=""
             />
-            <p>Mention légal</p>
-          </div>
-        </div>
-
-        <div>
-          <div class="flex-wrap">
-            <img
-              class="icon"
-              src="https://www.fine-s.fr/9959/test.jpg"
-              alt=""
-            />
-            <p>Mention légal</p>
-          </div>
-          <div class="flex-wrap">
-            <img
-              class="icon"
-              src="https://www.fine-s.fr/9959/test.jpg"
-              alt=""
-            />
-            <p>Mention légal</p>
+            <p class="space-start">Mention légal</p>
           </div>
         </div>
 
@@ -94,7 +75,7 @@
               src="https://www.fine-s.fr/9959/test.jpg"
               alt=""
             />
-            <p>Mention légal</p>
+            <p class="space-start">Mention légal</p>
           </div>
           <div class="flex-wrap">
             <img
@@ -102,14 +83,98 @@
               src="https://www.fine-s.fr/9959/test.jpg"
               alt=""
             />
-            <p>Mention légal</p>
+            <p class="space-start">Mention légal</p>
+          </div>
+        </div>
+
+        <div>
+          <div class="flex-wrap">
+            <img
+              class="icon"
+              src="https://www.fine-s.fr/9959/test.jpg"
+              alt=""
+            />
+            <p class="space-start">Mention légal</p>
+          </div>
+          <div class="flex-wrap">
+            <img
+              class="icon"
+              src="https://www.fine-s.fr/9959/test.jpg"
+              alt=""
+            />
+            <p class="space-start">Mention légal</p>
           </div>
         </div>
       </div>
-      <div></div>
     </nav>
   </header>
 
-  <main></main>
+  <!-- *** main *** -->
+
+  <main>
+    <div class="title">Mention légales</div>
+    <div>
+      <div class="div-space-title"></div>
+      <p>Le site internet oney.fr est edité par :</p>
+      <div class="paragraph-space"></div>
+      <p class="bold">Oney Bank</p>
+      <p>
+        Société Anonyme au capital de 51 286 585 € <br />
+        Siège social : 34 avenue de Flandre 59170 CROIX. <br />
+        Immatriculée au Registre du Commerce et des Sociétés de Lille Métropole
+        sous le numéro 546 380 197. <br />
+        Numéro d'identification de TVA Intracommunautaire : FR71546380197
+      </p>
+      <div class="paragraph-space"></div>
+      <p>
+        Oney Bank est agréée en tant qu'établissement de crédit par l'Autorité
+        de Contrôle Prudentiel (ACPR), 4 Place de Budapest, CS 92459, 75436
+        Paris Cedex 09, et est soumise à son contrôle. Oney Bank est également
+        inscrite sur la liste des prestataires de services d'investissement. La
+        liste des établissements de crédit agréés est consultable sur le site
+        Internet de la Banque de France (www.banque-france.fr) à la rubrique «
+        supervision et réglementation bancaire ».
+      </p>
+      <div class="paragraph-space"></div>
+      <p>
+        Dans le cadre de son activité d'assurance, Oney Bank est enregistrée en
+        tant que société de courtage d'assurance et est immatriculée à ce titre
+        auprès de l'Organisme pour le Registre des Intermédiaires en assurance
+        (ORIAS) sous le numéro 07 023 261. Le registre est consultable sur le
+        site Internet
+        <a class="underline" href="https://www.orias.fr/">www.orias.fr</a>
+      </p>
+      <div class="paragraph-space"></div>
+      <div class="disp-flex">
+        <p class="bold">Représentant légal :</p>
+        <p class="space-start">Jean-Pierre VIBOUD, Directeur Général</p>
+      </div>
+      <div class="disp-flex">
+        <p class="bold">Directeur de la publication :</p>
+        <p class="space-start">Corinne HOCHART, Directrice Générale France</p>
+      </div>
+      <div class="disp-flex">
+        <p class="bold">Hébergeur :</p>
+        <p class="space-start">Oney Bank - 34 avenue de Flandre, 59170 Croix</p>
+      </div>
+      <div class="paragraph-space"></div>
+      <div class="disp-flex">
+        <p class="bold">Crédits photos :</p>
+        <p class="space-start">getty images, istock.</p>
+      </div>
+      <div class="paragraph-space"></div>
+      <div class="disp-flex">
+        <p class="bold">Pour nous contacter :</p>
+        <p class="space-start">
+          <a
+            class="underline"
+            href="https://www.oney.fr/site/b/informations-legales/mentions-legales.html#"
+            >Cliquez ici</a
+          >
+        </p>
+      </div>
+      <div class="paragraph-space"></div>
+    </div>
+  </main>
   <footer></footer>
 </template>
